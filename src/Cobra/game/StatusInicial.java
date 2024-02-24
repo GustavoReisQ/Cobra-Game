e109dbdb9272927df934397dbd1dea9218742a8d
Gustavo <reis.gustavoqueiroz@gmail.com>
2024-02-23 22:16:15 -0300

    Teclado

--- src/Cobra/game/StatusInicial.java
+++ src/Cobra/game/StatusInicial.java
@@ -1,7 +1,11 @@
 package Cobra.game;
+import java.util.Scanner;
 
 public class StatusInicial {
     public static void main(String[] args) {
         System.out.println("Start");
+        Scanner teclado = new Scanner(System.in);
+
     }
+
 }
