bb618c7e90b6a5aedd92c0e2e51c74fd5f1328df
Gustavo <reis.gustavoqueiroz@gmail.com>
2024-02-24 08:24:37 -0300

    Teclado teste inicial

--- src/Cobra/game/StatusInicial.java
+++ src/Cobra/game/StatusInicial.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 public class StatusInicial {
     public static void main(String[] args) {
         System.out.println("Start");
+        System.out.println("Deseja iniciar o jogo? S/N");
         Scanner teclado = new Scanner(System.in);
         String resposta = teclado.next();
         if("S".equals(resposta)){
             System.out.println("Carregando jogo");
-        } else
-            System.out.println("Fechando");
+        } if ("N".equals(resposta)){
+            System.out.println("Fechando o jogo");
+        }
     }
-
 }
